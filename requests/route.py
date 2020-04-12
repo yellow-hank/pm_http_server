fa28dec364868fad97fb2a140f9b0b4e3f4606c2
yellow-hank <shps97040608@gmail.com>
2020-04-12 21:33:24 +0800

    Add one request for sending specific pm2.5 data

--- requests/route.py
+++ requests/route.py
@@ -1,16 +1,29 @@
 from flask import Blueprint, request, current_app, jsonify
-from .service import get_prob_single_sensor
+from .service import get_prob_single_sensor, get_specific_time_data
 
 requesting = Blueprint("requesting", __name__, url_prefix="/requesting")
 
 # Get the probability of green orange red
 @requesting.route("/prob/<int:campus_id>", methods=["GET"])
 def init(campus_id):
-  try:
-    data = get_prob_single_sensor(campus_id)
-    return jsonify(data)
-  except AttributeError as err:
-    current_app.logger.info(err)
-    result = "The request may not have the header of application/json"
-    return result, 400
+    try:
+        data = get_prob_single_sensor(campus_id)
+        return jsonify(data)
+    except AttributeError as err:
+        current_app.logger.info(err)
+        result = "The request may not have the header of application/json"
+        return result, 400
+
+
+
+# Get 8am 12pm 6pm pm25 data for each sensor
+@requesting.route("/specific/<int:campus_id>", methods=["GET"])
+def specific(campus_id):
+    try:
+        data = get_specific_time_data(campus_id)
+        return jsonify(data)
+    except AttributeError as err:
+        current_app.logger.info(err)
+        result = "The request may not have the header of application/json"
+        return result, 400
   
--- requests/service.py
+++ requests/service.py
@@ -30,7 +30,7 @@ def get_prob_single_sensor(campus_id):
                 ]
         }))
         #recentData = list(target_data.find({}, { '_id': 0 }))
-        
+        print (date_time)
         green =[0]*24
         yellow = [0]*24
         orange = [0] *24
@@ -85,6 +85,66 @@ def get_prob_single_sensor(campus_id):
         if len(recentData) == 0:
             raise QueryNotFound
         return prob_list
+    except QueryNotFound as err:
+        current_app.logger.info(err)
+        # return the an empty arr
+        return []
+
+def get_specific_time_data(campus_id):
+    try:
+        # prevent circular import
+        from flask_server import MONGO
+        target_data = MONGO.db.pm_data
+        half = timedelta(days=1)
+        #use utc because mongo use utc 
+        now = datetime.utcnow()
+        time_range=[8,12,18]
+        specific_time_data = []
+        for day in range(1,4):
+            for time in range(3):
+                lower = now.replace(hour = time_range[time]-8, minute = 0, second = 0) 
+                upper = now.replace(hour = time_range[time]-7, minute = 0, second = 0) 
+                lower -= half * day
+                upper -= half * day
+                
+
+                
+
+                pmdata = list(target_data.find({
+                            '$and': [
+                                {
+                                    'position': campus_id
+                                },
+                                {
+                                    'date': {
+                                        '$gte': lower,
+                                        '$lte': upper
+                                    }
+                                }
+                            ]
+                    }))
+
+                sum = 0
+                for i in pmdata:
+                    sum +=i["pm25"]
+                
+                #Change time format to "Year-Month-day Hour:minute:second"
+                taiwan_aware = lower.replace(hour = time_range[time], minute = 0, second = 0).strftime("%Y-%m-%d %H:%M:%S")
+
+                if(len(pmdata) == 0):
+                    specific_time_data.append({
+                        "pm25":float(sum),
+                        "date":taiwan_aware
+                    })
+                else:
+                    specific_time_data.append({
+                        "pm25":float(sum)/len(pmdata),
+                        "date": taiwan_aware
+                    })
+
+        if len(pmdata) == 0:
+                raise QueryNotFound
+        return specific_time_data
     except QueryNotFound as err:
         current_app.logger.info(err)
         # return the an empty arr
